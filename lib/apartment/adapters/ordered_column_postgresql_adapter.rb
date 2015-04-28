727815ebac7ac1810583b9115f3c002904d4f0cd
Brad Robertson <dev+brad@influitive.com>
2015-04-28 10:09:05 -0400

    support all versions of apartment

--- lib/apartment/adapters/ordered_column_postgresql_adapter.rb
+++ lib/apartment/adapters/ordered_column_postgresql_adapter.rb
@@ -6,4 +6,10 @@ module Apartment
       postgresql_adapter(config)
     end
   end
+
+  module Database
+    def self.ordered_column_postgresql_adapter(config)
+      postgresql_adapter(config)
+    end
+  end
 end
